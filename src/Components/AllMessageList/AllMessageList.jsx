7f56f0b033d9a210bf5e3fd710d34e2020254da6
V Krishna S Alluri <venkicv@hotmail.com>
2022-07-05 08:08:01 +0100

    UI tidy-up

--- src/Components/AllMessageList/AllMessageList.jsx
+++ src/Components/AllMessageList/AllMessageList.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import MessageList from '../MessageList/MessageList'
 import { useSelector } from 'react-redux'
+import './AllMessageList.css'
 
 const AllMessageList = () => {
   const { messagesWithMembersList } = useSelector((state) => state.messagesData)
 
   return (
-    <ul>
+    <ul className="message-list">
       <MessageList list={messagesWithMembersList} />
     </ul>
   )
